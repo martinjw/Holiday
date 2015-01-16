0df2e72ea3c58d1446ae90196781b5f90d9070b8
Martin Willey <martinjwilley@gmail.com>
2015-01-16 17:44:02 +0100

    Bump version to 1.1.1

--- tests/PublicHolidayTests/Properties/AssemblyInfo.cs
+++ tests/PublicHolidayTests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Martin Willey")]
 [assembly: AssemblyProduct("HolidayTests")]
-[assembly: AssemblyCopyright("Copyright © Martin Willey 2013")]
+[assembly: AssemblyCopyright("Copyright © Martin Willey 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
