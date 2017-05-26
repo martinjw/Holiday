45a748b4541acf774221fdde9198514f9dc1091f
Mike Smellie <MikeSmellie@quickenloans.com>
2017-05-26 08:46:36 -0400

    Added ability to get holidays in a date range

---
+++ src/PublicHoliday/Holiday.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublicHoliday
+{
+    /// <summary>
+    /// A holiday
+    /// </summary>
+    public class Holiday
+    {
+        private IPublicHolidays holidayCalendar;
+
+        /// <summary>
+        /// Constructs the holiday
+        /// </summary>
+        /// <param name="holidayCalendar">A holiday calendar object</param>
+        /// <param name="date">The date of the current holiday</param>
+        /// <param name="name">The name of the current holiday</param>
+        public Holiday(IPublicHolidays holidayCalendar, DateTime date, string name)
+        {
+            this.holidayCalendar = holidayCalendar;
+            this.HolidayDate = date;
+            this.Name = name;
+        }
+
+        /// <summary>
+        /// The previous working day for the holiday
+        /// </summary>
+        public DateTime PreviousWorkingDay
+        {
+            get { return holidayCalendar.PreviousWorkingDay(HolidayDate); }
+        }
+
+        /// <summary>
+        /// The next working day after the holiday
+        /// </summary>
+        public DateTime NextWorkingDay
+        {
+            get { return holidayCalendar.NextWorkingDay(HolidayDate); }
+        }
+
+        /// <summary>
+        /// Date for the current holiday
+        /// </summary>
+        public DateTime HolidayDate { get; set; }
+
+        /// <summary>
+        /// Name of the current holiday
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
---
+++ tests/PublicHolidayTests/TestPublicHolidayBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PublicHolidayTests
+{
+    using System;
+    using PublicHoliday;
+
+    [TestClass]
+    public class TestPublicHolidayBase
+    {
+        [TestMethod]
+        public void TestUSAHolidayThreeYearDateRange()
+        {
+            var holidayCalendar = new USAPublicHoliday();
+            var actual = holidayCalendar.GetHolidaysInDateRange(new DateTime(2015, 1, 2), new DateTime(2017, 12, 25));
+            Assert.AreEqual(actual.Count, 29);
+        }
+    }
+}
