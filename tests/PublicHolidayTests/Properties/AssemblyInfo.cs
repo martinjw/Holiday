06501127999980c0b5c8f39876c70a07bea126fa
Martin Willey <martinjwilley@gmail.com>
2019-06-08 15:54:39 +0200

    Bump version for release

--- tests/PublicHolidayTests/Properties/AssemblyInfo.cs
+++ tests/PublicHolidayTests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Martin Willey")]
 [assembly: AssemblyProduct("HolidayTests")]
-[assembly: AssemblyCopyright("Copyright © Martin Willey 2018")]
+[assembly: AssemblyCopyright("Copyright © Martin Willey 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
