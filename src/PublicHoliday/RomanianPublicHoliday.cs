57ecfa5889660c53b07a0e69daf6e2d0509c5273
Mihai Gliga <mihaigliga21@gmail.com>
2021-10-19 14:28:17 +0300

    romanian holiday calendar & related test; Orthodox Easter calculator

---
+++ src/PublicHoliday/RomanianPublicHoliday.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace PublicHoliday
+{
+    /// <summary>
+    /// Holidays in Romanian. Source https://en.wikipedia.org/wiki/Public_holidays_in_Romania
+    /// </summary>
+    public class RomanianPublicHoliday : PublicHolidayBase
+    {
+        /// <summary>
+        /// Get a list of dates for all holidays in a year.
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public override IList<DateTime> PublicHolidays(int year)
+            => PublicHolidayNames(year)
+                .Select(x => x.Key.Date)
+                .OrderBy(x => x)
+                .ToList();
+
+        /// <summary>
+        /// Public holiday names in Romanian.
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public override IDictionary<DateTime, string> PublicHolidayNames(int year)
+        {
+            var holidayNames = new Dictionary<DateTime, string>();
+
+            var newYear = NewYear(year).ToArray();
+            holidayNames.Add(newYear[0], "Anul nou");
+            holidayNames.Add(newYear[1], "Anul nou");
+
+            holidayNames.Add(UnificationOfPrincipalities(year), "Unirea Principatelor Române");
+
+            var easter = HolidayCalculator.GetOrthodoxEaster(year).AddHours(1).AddMinutes(1); // to avoid duplicate key
+
+            holidayNames.Add(EasterFriday(easter), "Vinerea Mare");
+            holidayNames.Add(easter, "Paștele");
+            holidayNames.Add(EasterMonday(easter), "Paștele");
+
+            holidayNames.Add(LabourDay(year), "Ziua Muncii");
+
+            var childrenDay = ChildrenDay(year);
+            if (childrenDay.HasValue)
+                holidayNames.Add(childrenDay.Value, "Ziua Copilului");
+
+            var whitMonday = WhitMonday(easter).ToArray();
+            holidayNames.Add(whitMonday[0], "Rusaliile");
+            holidayNames.Add(whitMonday[1], "Rusaliile");
+
+            holidayNames.Add(SaintMaryDay(year), "Sfânta Maria Mare");
+
+            holidayNames.Add(SaintAndrewDay(year), "Sfântul Andrei");
+
+            holidayNames.Add(NationalDay(year), "Ziua Națională a României");
+
+            var christmas = ChristmasDay(year).ToArray();
+            holidayNames.Add(christmas[0], "Crăciunul");
+            holidayNames.Add(christmas[1], "Crăciunul");
+
+            return holidayNames;
+        }
+
+        /// <summary>
+        /// Check if a specific date is a public holiday
+        /// </summary>
+        /// <param name="dt">The date you wish to check</param>
+        /// <returns>True if date is a public holiday</returns>
+        public override bool IsPublicHoliday(DateTime dt)
+        {
+            var year = dt.Year;
+            var date = dt.Date;
+
+            var holidays = PublicHolidays(year);
+
+            foreach (var holiday in holidays)
+            {
+                if (holiday == date) return true;
+            }
+
+            return false;
+        }
+
+        #region private
+
+        /// <summary>
+        /// New Year (Anul nou)
+        /// </summary>
+        /// <param name="year">The year</param>
+        /// <returns>Dates of given year</returns>
+        public static IEnumerable<DateTime> NewYear(int year)
+            => new[] { new DateTime(year, 1, 1), new DateTime(year, 1, 2) };
+
+        /// <summary>
+        /// Day of the Unification of the Romanian Principalities (Unirea Principatelor Române)
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public static DateTime UnificationOfPrincipalities(int year)
+            => new DateTime(year, 1, 24);
+
+        /// <summary>
+        /// Easter Orthodox Good Friday (Vinerea Mare)
+        /// </summary>
+        /// <param name="easter">The easter date</param>
+        /// <returns></returns>
+        public static DateTime EasterFriday(DateTime easter)
+            => easter.AddDays(-2);
+
+        /// <summary>
+        /// Easter Monday
+        /// </summary>
+        /// <param name="easter">The easter date</param>
+        /// <returns></returns>
+        public static DateTime EasterMonday(DateTime easter)
+            => easter.AddDays(1);
+
+        /// <summary>
+        /// International Labour Day (Ziua Muncii)
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public static DateTime LabourDay(int year)
+            => new DateTime(year, 5, 1);
+
+        /// <summary>
+        /// Children Day (Ziua Copilului). Public holiday starting with 2017
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public static DateTime? ChildrenDay(int year)
+        {
+            if (year >= 2017) return new DateTime(year, 6, 1);
+
+            return null;
+        }
+
+        /// <summary>
+        /// Easter Whit Monday (Rusaliile). 49 - Sunday (Descent of the Holy Spirit), 50 - Monday
+        /// </summary>
+        /// <param name="easter"></param>
+        /// <returns></returns>
+        public static IEnumerable<DateTime> WhitMonday(DateTime easter)
+            => new[] { easter.AddDays(49), easter.AddDays(50) };
+
+        /// <summary>
+        /// St Mary's Day (Adormirea Maicii Domnului/Sfânta Maria Mare)
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public static DateTime SaintMaryDay(int year)
+            => new DateTime(year, 8, 15);
+
+        /// <summary>
+        /// St. Andrew's Day (Sfântul Andrei)
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public static DateTime SaintAndrewDay(int year)
+            => new DateTime(year, 11, 30);
+
+        /// <summary>
+        /// National Day of Romania (Ziua Națională a României)
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public static DateTime NationalDay(int year)
+            => new DateTime(year, 12, 1);
+
+        /// <summary>
+        /// Christmas Day (Crăciunul)
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public static IEnumerable<DateTime> ChristmasDay(int year)
+            => new[] { new DateTime(year, 12, 25), new DateTime(year, 12, 26) };
+
+        #endregion
+    }
+}
---
+++ tests/PublicHolidayTests/TestRomanianPublicHoliday.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PublicHoliday;
+
+namespace PublicHolidayTests
+{
+    [TestClass]
+    public class TestRomanianPublicHoliday
+    {
+        [DataTestMethod]
+        [DataRow(1, 1, "New Year")]
+        [DataRow(1, 2, "New Year")]
+        [DataRow(1, 24, "Day of the Unification of the Romanian Principalities")]
+        [DataRow(4, 30, "Easter Orthodox Good Friday")]
+        [DataRow(5, 2, "Easter")]
+        [DataRow(5, 3, "Easter Monday")]
+        [DataRow(5, 1, "Labour Day")]
+        [DataRow(6, 1, "Children Day")]
+        [DataRow(6, 20, "Whit Day")]
+        [DataRow(6, 21, "Whit Monday")]
+        [DataRow(8, 15, "Saint Mary Day")]
+        [DataRow(11, 30, "Saint Andrew Day")]
+        [DataRow(12, 1, "National Day")]
+        [DataRow(12, 25, "Christmas Day")]
+        [DataRow(12, 26, "Christmas Next Day")]
+        public void TestRomanianHolidays2021(int month, int day, string name)
+        {
+            var holiday = new DateTime(2021, month, day);
+            var holidayCalendar = new RomanianPublicHoliday();
+
+            var actual = holidayCalendar.IsPublicHoliday(holiday);
+
+            Assert.IsTrue(actual, $"{holiday:D} is not a holiday - should be {name}");
+        }
+
+        [DataTestMethod]
+        [DataRow(2010, 4, 4, "Easter 2010")]
+        [DataRow(2015, 4, 12, "Easter 2015")]
+        [DataRow(2018, 4, 8, "Easter 2018")]
+        [DataRow(2019, 4, 28, "Easter 2019")]
+        [DataRow(2020, 4, 19, "Easter 2020")]
+        public void TestOrthodoxEasterRange2018_2019_2020(int year, int month, int day, string name)
+        {
+            var holiday = new DateTime(year, month, day);
+            var holidayCalendar = new RomanianPublicHoliday();
+
+            var actual = holidayCalendar.IsPublicHoliday(holiday);
+
+            Assert.IsTrue(actual, $"{holiday:D} is not a holiday - should be {name}");
+        }
+
+        /// <summary>
+        /// Before 2016 Children Day was not a public holiday.
+        /// Expect 14 items in list
+        /// </summary>
+        [TestMethod]
+        public void TestRomanianHolidays2016List()
+        {
+            var holidayCalendar = new RomanianPublicHoliday();
+            var holidays = holidayCalendar.PublicHolidayNames(2016);
+
+            Assert.IsTrue(14 == holidays.Count, "Should be 14 holidays in 2016");
+        }
+
+        /// <summary>
+        /// After 2017 Children Day was added as a public holiday.
+        /// Expect 15 items in list
+        /// </summary>
+        [TestMethod]
+        public void TestRomanianHolidays2017List()
+        {
+            var holidayCalendar = new RomanianPublicHoliday();
+            var holidays = holidayCalendar.PublicHolidayNames(2017);
+
+            Assert.IsTrue(15 == holidays.Count, "Should be 15 holidays in 2017");
+        }
+    }
+}
