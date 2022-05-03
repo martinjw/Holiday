40fd0b4b0a7c8d4d8132374232cf895432611632
Martin Willey <martinjwilley@gmail.com>
2022-05-03 18:41:23 +0200

    #67 Switzerland spelling error

--- src/PublicHoliday/Properties/AssemblyInfo.cs
+++ src/PublicHoliday/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Martin Willey")]
 [assembly: AssemblyProduct("Holiday")]
-[assembly: AssemblyCopyright("Copyright © Martin Willey 2021")]
+[assembly: AssemblyCopyright("Copyright © Martin Willey 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
