c3a3007358d8ca4a7de5cb2dd004505728c459b6
martinjw <martinwilley@hotmail.com>
2023-01-07 13:39:47 +0100

    Bump version

--- src/PublicHoliday/Properties/AssemblyInfo.cs
+++ src/PublicHoliday/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Martin Willey")]
 [assembly: AssemblyProduct("Holiday")]
-[assembly: AssemblyCopyright("Copyright © Martin Willey 2022")]
+[assembly: AssemblyCopyright("Copyright © Martin Willey 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.22.0.0")]
-[assembly: AssemblyFileVersion("2.22.0.0")]
+[assembly: AssemblyVersion("2.23.0.0")]
+[assembly: AssemblyFileVersion("2.23.0.0")]
 
 // Visibility of project of test
 [assembly: InternalsVisibleTo("PublicHolidayTests")]
