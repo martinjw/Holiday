03dae5c993ae22e3ef8a0978419179632ba8542f
Teemu Laine <16613890+vergi40@users.noreply.github.com>
2023-01-07 14:29:35 +0200

    Add Finland holidays (#83)
    * Finland: Add public holidays
    
    * Finland: Add holiday alterations to prolong support back to 1774
    
    * Finland: Add unit tests

---
+++ src/PublicHoliday/FinlandPublicHoliday.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+// ReSharper disable StringLiteralTypo
+
+namespace PublicHoliday
+{
+    /// <summary>
+    /// Public holidays in Finland.
+    /// <br />
+    /// Typically the large holidays are already celebrated on the Eve. These can be added in constructor.
+    /// <br />
+    /// Public holiday dates have had minor alterations. Current implementation should be quite accurate back to 1774.
+    /// <br />
+    /// Sources used:
+    /// https://en.wikipedia.org/wiki/Public_holidays_in_Finland
+    /// https://fi.wikipedia.org/wiki/Pyh%C3%A4p%C3%A4iv%C3%A4
+    /// </summary>
+    public class FinlandPublicHoliday : PublicHolidayBase
+    {
+        /// <summary>
+        /// Include days that are considered holiday in some labor agreements. Relevant as some businesses
+        /// are partly or fully closed and public transport runs on special holiday schedule.
+        /// </summary>
+        private readonly bool _includeNonOfficialHolidays;
+
+        /// <summary>
+        /// Became public holiday 1919
+        /// https://fi.wikipedia.org/wiki/Suomen_itsen%C3%A4isyysp%C3%A4iv%C3%A4
+        /// </summary>
+        private const int IndependenceDayAsHolidaySince = 1919;
+
+        /// <summary>
+        /// Before 1955 midsummer was celebrated ~half year before christmas (fixed 23.6. and 24.6.)
+        /// Since 1955 it's been movable saturday.
+        /// https://fi.wikipedia.org/wiki/Juhannus
+        /// </summary>
+        private const int MidsummerInSaturdaySince = 1955;
+
+        /// <summary>
+        /// Before 1955 All Saints Day was fixed in 1.11.
+        /// Since 1955 it's been movable saturday.
+        /// </summary>
+        private const int AllSaintsDayInSaturdaySince = 1955;
+        
+        /// <summary>
+        /// Public holidays of Finland
+        /// </summary>
+        /// <param name="includeNonOfficialHolidays">
+        /// Midsummer Eve and Christmas Eve are non-official holidays. They are considered
+        /// holiday in some labor agreements</param>
+        public FinlandPublicHoliday(bool includeNonOfficialHolidays = false)
+        {
+            _includeNonOfficialHolidays = includeNonOfficialHolidays;
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override IList<DateTime> PublicHolidays(int year)
+        {
+            return PublicHolidayNames(year).Keys.OrderBy(x => x).ToList();
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override IDictionary<DateTime, string> PublicHolidayNames(int year)
+        {
+            // Fixed dates
+            var names = new Dictionary<DateTime, string>
+            {
+                { new DateTime(year, 1, 1), "Uudenvuodenpäivä" },
+                { new DateTime(year, 1, 6), "Loppiainen" },
+                { new DateTime(year, 5, 1), "Vappu" },
+                { new DateTime(year, 12, 25), "Joulupäivä" },
+                { new DateTime(year, 12, 26), "Tapaninpäivä" },
+            };
+
+            if (year >= IndependenceDayAsHolidaySince)
+            {
+                names.Add(new DateTime(year, 12, 6), "Itsenäisyyspäivä");
+            }
+
+            // Easter holidays: friday, sunday, monday
+            var easter = HolidayCalculator.GetEaster(year);
+            names.Add(easter.AddDays(-2), "Pitkäperjantai");
+            names.Add(easter, "Pääsiäispäivä");
+            names.Add(easter.AddDays(1), "2. pääsiäispäivä");
+
+            // Ascension, 39 days after easter
+            // Some leap years might overlap with May Day (vappu)
+            if (!names.ContainsKey(easter.AddDays(39)))
+            {
+                names.Add(easter.AddDays(39), "Helatorstai");
+            }
+
+            // Pentecost sunday, 49 days after easter
+            names.Add(easter.AddDays(49), "Helluntaipäivä");
+
+            // Moveable dates
+            names.Add(GetMidsummer(year), "Juhannuspäivä");
+            names.Add(GetAllSaintsDay(year), "Pyhäinpäivä");
+
+            // Non-official
+            if (_includeNonOfficialHolidays)
+            {
+                names.Add(GetMidsummer(year).AddDays(-1), "Juhannusaatto");
+                names.Add(new DateTime(year, 12, 24), "Jouluaatto");
+            }
+
+            return names;
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override bool IsPublicHoliday(DateTime dt)
+        {
+            // Normalize
+            var date = new DateTime(dt.Year, dt.Month, dt.Day);
+
+            return PublicHolidays(dt.Year).Contains(date);
+        }
+
+        /// <summary>
+        /// Saturday between 20. and 26. June
+        /// </summary>
+        private DateTime GetMidsummer(int year)
+        {
+            if(year >= MidsummerInSaturdaySince)
+            {
+                var date = new DateTime(year, 6, 20);
+                return HolidayCalculator.FindNext(date, DayOfWeek.Saturday);
+            }
+            else
+            {
+                return new DateTime(year, 6, 24);
+            }
+        }
+
+        /// <summary>
+        /// Saturday between 31 October and 6 November
+        /// </summary>
+        private DateTime GetAllSaintsDay(int year)
+        {
+            if(year >= AllSaintsDayInSaturdaySince)
+            {
+                var date = new DateTime(year, 10, 31);
+                return HolidayCalculator.FindNext(date, DayOfWeek.Saturday);
+            }
+            else
+            {
+                return new DateTime(year, 11, 1);
+            }
+        }
+    }
+}
---
+++ tests/PublicHolidayTests/TestFinlandPublicHoliday.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PublicHoliday;
+// ReSharper disable StringLiteralTypo
+
+namespace PublicHolidayTests
+{
+    [TestClass]
+    public class TestFinlandPublicHoliday
+    {
+        [TestMethod]
+        public void Test2022FullCalendar()
+        {
+            // https://almanakka.helsinki.fi/en/flag-days-and-holidays-in-finland.html
+            var holidays = new List<DateTime>
+            {
+                new(2022, 1, 1),
+                new(2022, 1, 6),
+                new(2022, 4, 15),
+                new(2022, 4, 17),
+                new(2022, 4, 18),
+                new(2022, 5, 1),
+                new(2022, 5, 26),
+                new(2022, 6, 24),
+                new(2022, 6, 25),
+                new(2022, 11, 5),
+                new(2022, 12, 6),
+                new(2022, 12, 24),
+                new(2022, 12, 25),
+                new(2022, 12, 26)
+            };
+
+            var calendar = new FinlandPublicHoliday(true);
+
+            foreach (var date in holidays)
+            {
+                Assert.IsTrue(calendar.IsPublicHoliday(date));
+            }
+        }
+
+        [TestMethod]
+        public void Test2023FullCalentar()
+        {
+            // https://almanakka.helsinki.fi/en/flag-days-and-holidays-in-finland.html
+            var holidays = new List<DateTime>
+            {
+                new(2023, 1, 1),
+                new(2023, 1, 6),
+                new(2023, 4, 7),
+                new(2023, 4, 9),
+                new(2023, 4, 10),
+                new(2023, 5, 1),
+                new(2023, 5, 18),
+                new(2023, 6, 23),
+                new(2023, 6, 24),
+                new(2023, 11, 4),
+                new(2023, 12, 6),
+                new(2023, 12, 24),
+                new(2023, 12, 25),
+                new(2023, 12, 26)
+            };
+
+            var calendar = new FinlandPublicHoliday(true);
+
+            foreach (var date in holidays)
+            {
+                Assert.IsTrue(calendar.IsPublicHoliday(date));
+            }
+        }
+
+        // https://fi.wikipedia.org/wiki/Juhannus
+        [DataTestMethod]
+        [DataRow(2010, 6, 26)]
+        [DataRow(2011, 6, 25)]
+        [DataRow(2012, 6, 23)]
+        [DataRow(2013, 6, 22)]
+        [DataRow(2014, 6, 21)]
+        [DataRow(2015, 6, 20)]
+        [DataRow(2016, 6, 25)]
+        [DataRow(2017, 6, 24)]
+        [DataRow(2018, 6, 23)]
+        [DataRow(2019, 6, 22)]
+        public void TestMovingMidsummers(int year, int month, int day)
+        {
+            var calendar = new FinlandPublicHoliday(true);
+
+            var date = new DateTime(year, month, day);
+            Assert.IsTrue(calendar.IsPublicHoliday(date));
+
+            var holidays = calendar.PublicHolidayNames(year);
+            Assert.AreEqual("Juhannuspäivä", holidays[date]);
+            Assert.AreEqual("Juhannusaatto", holidays[date.AddDays(-1)]);
+        }
+
+        [DataTestMethod]
+        [DataRow(2023, 11, 4)]
+        [DataRow(2024, 11, 2)]
+        [DataRow(2025, 11, 1)]
+        public void TestMovingAllSaintsDay(int year, int month, int day)
+        {
+            var calendar = new FinlandPublicHoliday(true);
+
+            var date = new DateTime(year, month, day);
+            Assert.IsTrue(calendar.IsPublicHoliday(date));
+
+            var holidays = calendar.PublicHolidayNames(year);
+            Assert.AreEqual("Pyhäinpäivä", holidays[date]);
+        }
+
+        [DataTestMethod]
+        [DataRow(2020)]
+        [DataRow(2021)]
+        [DataRow(2022)]
+        [DataRow(2023)]
+        [DataRow(2024)]
+        [DataRow(2025)]
+        [DataRow(2026)]
+        [DataRow(2027)]
+        [DataRow(2028)]
+        [DataRow(2029)]
+        public void TestStaticDates(int year)
+        {
+            var staticHolidays = new List<DateTime>
+            {
+                new(year, 1, 1),
+                new(year, 1, 6),
+                new(year, 5, 1),
+                new(year, 12, 6),
+                new(year, 12, 25),
+                new(year, 12, 26)
+            };
+
+            var calendar = new FinlandPublicHoliday();
+
+            foreach (var holiday in staticHolidays)
+            {
+                Assert.IsTrue(calendar.IsPublicHoliday(holiday));
+            }
+        }
+
+        [TestMethod]
+        public void TestIsPublicHoliday_ShouldNormalizeInputDateTime()
+        {
+            var calendar = new FinlandPublicHoliday();
+            
+            var holiday = new DateTime(2023, 1, 1, 5, 0, 0);
+
+            Assert.IsTrue(calendar.IsPublicHoliday(holiday));
+
+        }
+        
+        [DataTestMethod]
+        [DataRow(2000, 13)]
+        [DataRow(2001, 13)]
+        [DataRow(2002, 13)]
+        [DataRow(2003, 13)]
+        [DataRow(2004, 13)]
+        [DataRow(2005, 13)] 
+        [DataRow(2006, 13)] 
+        [DataRow(2007, 13)] 
+        [DataRow(2008, 12)]//Alteration due to leap year 
+        [DataRow(2009, 13)] 
+        public void TestHolidayCountWithoutNonOfficials(int year, int expected)
+        {
+            var calendar = new FinlandPublicHoliday();
+
+            Assert.AreEqual(expected, calendar.PublicHolidays(year).Count);
+        }
+
+        [TestMethod]
+        public void TestCalendarAlterations()
+        {
+            var calendar = new FinlandPublicHoliday(true);
+
+            // Independence day
+            Assert.IsFalse(calendar.IsPublicHoliday(new DateTime(1918, 12, 6)));
+            Assert.IsTrue(calendar.IsPublicHoliday(new DateTime(1919, 12, 6)));
+
+            // Midsummer & All Saints Day
+            var holidays1954 = calendar.PublicHolidayNames(1954);
+            Assert.AreEqual("Juhannusaatto", holidays1954[new DateTime(1954, 6, 23)]);
+            Assert.AreEqual("Juhannuspäivä", holidays1954[new DateTime(1954, 6, 24)]);
+            Assert.AreEqual("Pyhäinpäivä", holidays1954[new DateTime(1954, 11, 1)]);
+
+            var holidays1955 = calendar.PublicHolidayNames(1955);
+            Assert.AreEqual("Juhannusaatto", holidays1955[new DateTime(1955, 6, 24)]);
+            Assert.AreEqual("Juhannuspäivä", holidays1955[new DateTime(1955, 6, 25)]);
+            Assert.AreEqual("Pyhäinpäivä", holidays1955[new DateTime(1955, 11, 5)]);
+        }
+
+        [TestMethod]
+        [Ignore("Test that moving days won't cause any unexpected failures")]
+        public void TestCalendarCreationFrom1744To2100()
+        {
+            for (int i = 1744; i <= 2100; i++)
+            {
+                var calendar = new FinlandPublicHoliday(true);
+
+                var holidays = calendar.PublicHolidayNames(i);
+            }
+        }
+    }
+}
