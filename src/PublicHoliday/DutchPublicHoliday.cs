0dd3ab9ee45e4d81144e0c4a0b6b23ba17d583a2
martinjw <martinwilley@hotmail.com>
2023-05-05 20:39:11 +0200

    Bump version to 2.28.0

--- src/PublicHoliday/DutchPublicHoliday.cs
+++ src/PublicHoliday/DutchPublicHoliday.cs
@@ -4,257 +4,257 @@ using System.Linq;
 
 namespace PublicHoliday
 {
-    /// <summary>
-    /// Holiday calendar for the Netherlands
-    /// </summary>
-    public class DutchPublicHoliday : PublicHolidayBase
-    {
-        private readonly bool _liberationDayOnlyAtLustrum = false;
+	/// <summary>
+	/// Holiday calendar for the Netherlands
+	/// </summary>
+	public class DutchPublicHoliday : PublicHolidayBase
+	{
+		private readonly bool _liberationDayOnlyAtLustrum = false;
 
-        #region Individual Holidays
+		#region Individual Holidays
 
-        /// <summary>
-        /// New Year's Day January 1 - Nieuwjaarsdag
-        /// </summary>
-        /// <param name="year">The year.</param>
-        /// <returns>Date of in the given year.</returns>
-        public static DateTime NewYear(int year)
-        {
-            return new DateTime(year, 1, 1);
-        }
+		/// <summary>
+		/// New Year's Day January 1 - Nieuwjaarsdag
+		/// </summary>
+		/// <param name="year">The year.</param>
+		/// <returns>Date of in the given year.</returns>
+		public static DateTime NewYear(int year)
+		{
+			return new DateTime(year, 1, 1);
+		}
 
-        /// <summary>
-        /// Easter Monday 1st Monday after Easter - Paasmaandag
-        /// </summary>
-        /// <param name="year">The year.</param>
-        /// <returns>Date of in the given year.</returns>
-        public static DateTime EasterMonday(int year)
-        {
-            var hol = HolidayCalculator.GetEaster(year);
-            hol = hol.AddDays(1);
-            return hol;
-        }
+		/// <summary>
+		/// Easter Monday 1st Monday after Easter - Paasmaandag
+		/// </summary>
+		/// <param name="year">The year.</param>
+		/// <returns>Date of in the given year.</returns>
+		public static DateTime EasterMonday(int year)
+		{
+			var hol = HolidayCalculator.GetEaster(year);
+			hol = hol.AddDays(1);
+			return hol;
+		}
 
-        private static DateTime EasterMonday(DateTime easter)
-        {
-            return easter.AddDays(1);
-        }
+		private static DateTime EasterMonday(DateTime easter)
+		{
+			return easter.AddDays(1);
+		}
 
-        /// <summary>
-        /// Kingsday/Queensday - Koningsdag/Koninginnendag
-        /// </summary>
-        /// <param name="year">The year.</param>
-        /// <returns>Date of in the given year.</returns>
-        public static DateTime KingsDay(int year)
-        {
-            var date = new DateTime(year, 4, 27);
-            if (year < 1949)
-            {
-                date = new DateTime(year, 8, 31);
-            }
-            else if (year < 2014)
-            {
-                date = new DateTime(year, 4, 30);
-            }
+		/// <summary>
+		/// Kingsday/Queensday - Koningsdag/Koninginnendag
+		/// </summary>
+		/// <param name="year">The year.</param>
+		/// <returns>Date of in the given year.</returns>
+		public static DateTime KingsDay(int year)
+		{
+			var date = new DateTime(year, 4, 27);
+			if (year < 1949)
+			{
+				date = new DateTime(year, 8, 31);
+			}
+			else if (year < 2014)
+			{
+				date = new DateTime(year, 4, 30);
+			}
 
-            if (date.DayOfWeek == DayOfWeek.Sunday)
-            {
-                date = date.AddDays(-1);
-            }
+			if (date.DayOfWeek == DayOfWeek.Sunday)
+			{
+				date = date.AddDays(-1);
+			}
 
-            return date;
-        }
+			return date;
+		}
 
-        /// <summary>
-        /// Liberation Day May 5 - Bevrijdingsdag
-        /// </summary>
-        /// <param name="year">The year.</param>
-        /// <param name="onlyAtLustrum">Specify if LiberationDay has to be a holiday only at lustrum years</param>
-        /// <returns>Date of in the given year.</returns>
-        public static DateTime? LiberationDay(int year, bool onlyAtLustrum = false)
-        {
-            if (year >= 2020 && onlyAtLustrum) // since 2020 it depends on the sector
-            {
-                if (year % 5 == 0)
-                {
-                    return new DateTime(year, 5, 5);
-                }
-                return null;
-            }
+		/// <summary>
+		/// Liberation Day May 5 - Bevrijdingsdag
+		/// </summary>
+		/// <param name="year">The year.</param>
+		/// <param name="onlyAtLustrum">Specify if LiberationDay has to be a holiday only at lustrum years</param>
+		/// <returns>Date of in the given year.</returns>
+		public static DateTime? LiberationDay(int year, bool onlyAtLustrum = false)
+		{
+			if (year >= 2020 && onlyAtLustrum) // since 2020 it depends on the sector
+			{
+				if (year % 5 == 0)
+				{
+					return new DateTime(year, 5, 5);
+				}
+				return null;
+			}
 
-            if (year >= 1990) //annual since 1990
-            {
-                return new DateTime(year, 5, 5);
-            }
+			if (year >= 1990) //annual since 1990
+			{
+				return new DateTime(year, 5, 5);
+			}
 
-            if (year >= 1945) //introduced in 1945 every 5 years
-            {
-                if (year % 5 == 0)
-                {
-                    return new DateTime(year, 5, 5);
-                }
-            }
-            return null;
-        }
+			if (year >= 1945) //introduced in 1945 every 5 years
+			{
+				if (year % 5 == 0)
+				{
+					return new DateTime(year, 5, 5);
+				}
+			}
+			return null;
+		}
 
-        /// <summary>
-        /// Ascension 6th Thursday after Easter - Hemelvaartsdag
-        /// </summary>
-        /// <param name="year">The year.</param>
-        /// <returns>Date of in the given year.</returns>
-        public static DateTime Ascension(int year)
-        {
-            var hol = HolidayCalculator.GetEaster(year);
-            hol = hol.AddDays(4 + (7 * 5));
-            return hol;
-        }
+		/// <summary>
+		/// Ascension 6th Thursday after Easter - Hemelvaartsdag
+		/// </summary>
+		/// <param name="year">The year.</param>
+		/// <returns>Date of in the given year.</returns>
+		public static DateTime Ascension(int year)
+		{
+			var hol = HolidayCalculator.GetEaster(year);
+			hol = hol.AddDays(4 + (7 * 5));
+			return hol;
+		}
 
-        private static DateTime Ascension(DateTime easter)
-        {
-            return easter.AddDays(4 + (7 * 5));
-        }
+		private static DateTime Ascension(DateTime easter)
+		{
+			return easter.AddDays(4 + (7 * 5));
+		}
 
-        /// <summary>
-        /// Whit Monday - Pentecost Monday 7th Monday after Easter - Pinkstermaandag
-        /// </summary>
-        /// <param name="year"></param>
-        /// <returns></returns>
-        public static DateTime PentecostMonday(int year)
-        {
-            var hol = HolidayCalculator.GetEaster(year);
-            hol = hol.AddDays((7 * 7) + 1);
-            return hol;
-        }
+		/// <summary>
+		/// Whit Monday - Pentecost Monday 7th Monday after Easter - Pinkstermaandag
+		/// </summary>
+		/// <param name="year"></param>
+		/// <returns></returns>
+		public static DateTime PentecostMonday(int year)
+		{
+			var hol = HolidayCalculator.GetEaster(year);
+			hol = hol.AddDays((7 * 7) + 1);
+			return hol;
+		}
 
-        private static DateTime PentecostMonday(DateTime easter)
-        {
-            return easter.AddDays(1 + (7 * 7));
-        }
+		private static DateTime PentecostMonday(DateTime easter)
+		{
+			return easter.AddDays(1 + (7 * 7));
+		}
 
-        /// <summary>
-        /// Christmas - December 25 - Eerste Kerstdag
-        /// </summary>
-        /// <param name="year"></param>
-        /// <returns></returns>
-        public static DateTime Christmas(int year)
-        {
-            return new DateTime(year, 12, 25);
-        }
+		/// <summary>
+		/// Christmas - December 25 - Eerste Kerstdag
+		/// </summary>
+		/// <param name="year"></param>
+		/// <returns></returns>
+		public static DateTime Christmas(int year)
+		{
+			return new DateTime(year, 12, 25);
+		}
 
-        /// <summary>
-        /// Boxing Day - December 26 - Tweede Kerstdag
-        /// </summary>
-        /// <param name="year"></param>
-        /// <returns></returns>
-        public static DateTime BoxingDay(int year)
-        {
-            return new DateTime(year, 12, 26);
-        }
+		/// <summary>
+		/// Boxing Day - December 26 - Tweede Kerstdag
+		/// </summary>
+		/// <param name="year"></param>
+		/// <returns></returns>
+		public static DateTime BoxingDay(int year)
+		{
+			return new DateTime(year, 12, 26);
+		}
 
-        #endregion
+		#endregion
 
-        /// <summary>
+		/// <summary>
 		/// Default - includes Liberation Day as default
 		/// </summary>
 		public DutchPublicHoliday()
-        {
-        }
+		{
+		}
 
-        /// <summary>
-        /// Specify whether to include Liberation Day only at lustrum years
-        /// </summary>
-        /// <param name="liberationDayOnlyAtLustrum"></param>
-        public DutchPublicHoliday(bool liberationDayOnlyAtLustrum)
-        {
-            _liberationDayOnlyAtLustrum = liberationDayOnlyAtLustrum;
-        }
+		/// <summary>
+		/// Specify whether to include Liberation Day only at lustrum years
+		/// </summary>
+		/// <param name="liberationDayOnlyAtLustrum"></param>
+		public DutchPublicHoliday(bool liberationDayOnlyAtLustrum)
+		{
+			_liberationDayOnlyAtLustrum = liberationDayOnlyAtLustrum;
+		}
 
-        /// <summary>
-        /// Get a list of dates for all holidays in a year.
-        /// </summary>
-        /// <param name="year">The year</param>
-        /// <returns>List of public holidays</returns>
-        public override IList<DateTime> PublicHolidays(int year)
-        {
-            return PublicHolidayNames(year).Select(x => x.Key).OrderBy(x => x).ToList();
-        }
+		/// <summary>
+		/// Get a list of dates for all holidays in a year.
+		/// </summary>
+		/// <param name="year">The year</param>
+		/// <returns>List of public holidays</returns>
+		public override IList<DateTime> PublicHolidays(int year)
+		{
+			return PublicHolidayNames(year).Select(x => x.Key).OrderBy(x => x).ToList();
+		}
 
-        /// <summary>
-        /// Public holiday names in Dutch.
-        /// </summary>
-        /// <param name="year">The year.</param>
-        /// <returns></returns>
-        public override IDictionary<DateTime, string> PublicHolidayNames(int year)
-        {
-            var bHols = new Dictionary<DateTime, string>();
-            bHols.Add(NewYear(year), "Nieuwjaar");
-            DateTime easter = HolidayCalculator.GetEaster(year);
-            bHols.Add(EasterMonday(easter), "Paasmaandag");
-            bHols.Add(KingsDay(year), "Koningsdag");
-            var liberationDay = LiberationDay(year, _liberationDayOnlyAtLustrum);
-            if (liberationDay != null)
-            {
-                bHols.Add(liberationDay.Value, "Bevrijdingsdag");
-            }
+		/// <summary>
+		/// Public holiday names in Dutch.
+		/// </summary>
+		/// <param name="year">The year.</param>
+		/// <returns></returns>
+		public override IDictionary<DateTime, string> PublicHolidayNames(int year)
+		{
+			var bHols = new Dictionary<DateTime, string>();
+			bHols.Add(NewYear(year), "Nieuwjaar");
+			DateTime easter = HolidayCalculator.GetEaster(year);
+			bHols.Add(EasterMonday(easter), "Paasmaandag");
+			bHols.Add(KingsDay(year), "Koningsdag");
+			var liberationDay = LiberationDay(year, _liberationDayOnlyAtLustrum);
+			if (liberationDay != null)
+			{
+				bHols.Add(liberationDay.Value, "Bevrijdingsdag");
+			}
 
-            var ascension = Ascension(easter);
-            if (bHols.ContainsKey(ascension)) ascension = ascension.AddSeconds(1);
-            bHols.Add(ascension, "Hemelvaartsdag");
-            bHols.Add(PentecostMonday(easter), "Pinkstermaandag");
-            bHols.Add(Christmas(year), "Eerste Kerstdag");
-            bHols.Add(BoxingDay(year), "Tweede Kerstdag");
-            return bHols;
-        }
+			var ascension = Ascension(easter);
+			if (bHols.ContainsKey(ascension)) ascension = ascension.AddSeconds(1);
+			bHols.Add(ascension, "Hemelvaartsdag");
+			bHols.Add(PentecostMonday(easter), "Pinkstermaandag");
+			bHols.Add(Christmas(year), "Eerste Kerstdag");
+			bHols.Add(BoxingDay(year), "Tweede Kerstdag");
+			return bHols;
+		}
 
-        /// <summary>
-        /// Check if a specific date is a public holiday.
-        /// Obviously the PublicHoliday list is more efficient for repeated checks
-        /// Note holidays can fall on weekends and there is no fixed moving of such dates.
-        /// </summary>
-        /// <param name="dt">The date you wish to check</param>
-        /// <returns>True if date is a public holiday</returns>
-        public override bool IsPublicHoliday(DateTime dt)
-        {
-            var year = dt.Year;
-            var date = dt.Date;
+		/// <summary>
+		/// Check if a specific date is a public holiday.
+		/// Obviously the PublicHoliday list is more efficient for repeated checks
+		/// Note holidays can fall on weekends and there is no fixed moving of such dates.
+		/// </summary>
+		/// <param name="dt">The date you wish to check</param>
+		/// <returns>True if date is a public holiday</returns>
+		public override bool IsPublicHoliday(DateTime dt)
+		{
+			var year = dt.Year;
+			var date = dt.Date;
 
-            switch (dt.Month)
-            {
-                case 1:
-                    if (NewYear(year) == date)
-                        return true;
-                    break;
-                case 3:
-                case 4:
-                case 8:
-                    if (EasterMonday(year) == date)
-                        return true;
-                    if (KingsDay(year) == date) // Usually April 27, but historically also in August
-                        return true;
-                    break;
-                case 5:
-                    if (LiberationDay(year, _liberationDayOnlyAtLustrum) == date)
-                        return true;
-                    if (Ascension(year) == date)
-                        return true; // usually in May (may 25, 2006)
-                    if (PentecostMonday(year) == date)
-                        return true; // May 20 2004
-                    break;
-                case 6:
-                    if (Ascension(year) == date)
-                        return true; // Ascension was June 1 2000
-                    if (PentecostMonday(year) == date)
-                        return true; // June 12 2006
-                    break;
-                case 12:
-                    if (Christmas(year) == date)
-                        return true;
-                    if (BoxingDay(year) == date)
-                        return true;
-                    break;
-            }
+			switch (dt.Month)
+			{
+				case 1:
+					if (NewYear(year) == date)
+						return true;
+					break;
+				case 3:
+				case 4:
+				case 8:
+					if (EasterMonday(year) == date)
+						return true;
+					if (KingsDay(year) == date) // Usually April 27, but historically also in August
+						return true;
+					break;
+				case 5:
+					if (LiberationDay(year, _liberationDayOnlyAtLustrum) == date)
+						return true;
+					if (Ascension(year) == date)
+						return true; // usually in May (may 25, 2006)
+					if (PentecostMonday(year) == date)
+						return true; // May 20 2004
+					break;
+				case 6:
+					if (Ascension(year) == date)
+						return true; // Ascension was June 1 2000
+					if (PentecostMonday(year) == date)
+						return true; // June 12 2006
+					break;
+				case 12:
+					if (Christmas(year) == date)
+						return true;
+					if (BoxingDay(year) == date)
+						return true;
+					break;
+			}
 
-            return false;
-        }
-    }
+			return false;
+		}
+	}
 }
