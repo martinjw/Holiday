2e609e5fa2088353edd2a49b5dd25dc608c34590
Eales <46241595+Eales@users.noreply.github.com>
2025-01-03 22:25:11 +0100

    Update PolandPublicHoliday.cs

--- src/PublicHoliday/PolandPublicHoliday.cs
+++ src/PublicHoliday/PolandPublicHoliday.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,240 +12,135 @@ namespace PublicHoliday
         #region Individual Holidays
 
         /// <summary>
-        /// Nowy Rok - New Year's Day
+        /// New Year's Day (1st January)
         /// </summary>
-        /// <param name="year">The year.</param>
-        /// <returns>Date of in the given year.</returns>
-        public static DateTime NewYear(int year)
-        {
-            return new DateTime(year, 1, 1);
-        }
+        public static DateTime NewYear(int year) => new DateTime(year, 1, 1);
 
         /// <summary>
-        /// Swieto Trzech Króli - Epiphany January 6
+        /// Epiphany (6th January)
         /// </summary>
-        /// <param name="year"></param>
-        public static DateTime Epiphany(int year)
-        {
-            return new DateTime(year, 1, 6);
-        }
+        public static DateTime Epiphany(int year) => new DateTime(year, 1, 6);
 
         /// <summary>
-        /// Poniedzialek Wielkanocny - Easter Monday
+        /// Easter Sunday (calculated dynamically)
         /// </summary>
-        /// <param name="year">The year.</param>
-        /// <returns>Date of in the given year.</returns>
-        public static DateTime EasterMonday(int year)
-        {
-            var hol = HolidayCalculator.GetEaster(year);
-            hol = hol.AddDays(1);
-            return hol;
-        }
-
-        private static DateTime EasterMonday(DateTime easter)
-        {
-            return easter.AddDays(1);
-        }
+        public static DateTime EasterSunday(int year) => HolidayCalculator.GetEaster(year);
 
         /// <summary>
-        /// Swieto Panstwowe - Labour Day
+        /// Easter Monday (day after Easter Sunday)
         /// </summary>
-        /// <param name="year">The year.</param>
-        public static DateTime LabourDay(int year)
-        {
-            return new DateTime(year, 5, 1);
-        }
+        public static DateTime EasterMonday(int year) => EasterSunday(year).AddDays(1);
 
         /// <summary>
-        /// Swieto Narodowe Trzeciego Maja - Constitution Day
+        /// Labour Day (1st May)
         /// </summary>
-        /// <param name="year">The year.</param>
-        public static DateTime ConstitutionDay(int year)
-        {
-            return new DateTime(year, 5, 3);
-        }
+        public static DateTime LabourDay(int year) => new DateTime(year, 5, 1);
 
         /// <summary>
-        /// dzien Bozego Ciala - CorpusChristi
+        /// Constitution Day (3rd May)
         /// </summary>
-        /// <param name="year"></param>
-
-        public static DateTime CorpusChristi(int year)
-        {
-            var hol = HolidayCalculator.GetEaster(year);
-            //first Thursday after Trinity Sunday (Pentecost + 1 week)
-            hol = hol.AddDays((7 * 8) + 4);
-            return hol;
-        }
+        public static DateTime ConstitutionDay(int year) => new DateTime(year, 5, 3);
 
         /// <summary>
-        /// Wniebowziecie Najswietszej Maryi Panny - Assumption of Mary
+        /// Pentecost (49 days after Easter Sunday)
         /// </summary>
-        /// <param name="year"></param>
-        public static DateTime Assumption(int year)
-        {
-            return new DateTime(year, 8, 15);
-        }
+        public static DateTime Pentecost(int year) => EasterSunday(year).AddDays(49);
 
         /// <summary>
-        /// Wszystkich Swietych - All Saints
+        /// Corpus Christi (60 days after Easter Sunday)
         /// </summary>
-        /// <param name="year"></param>
-
-        public static DateTime AllSaints(int year)
-        {
-            return new DateTime(year, 11, 1);
-        }
+        public static DateTime CorpusChristi(int year) => EasterSunday(year).AddDays(60);
 
         /// <summary>
-        /// Narodowe Swieto Niepodleglosci - Independence Day
+        /// Assumption of Mary (15th August)
         /// </summary>
-        /// <param name="year"></param>
-
-        public static DateTime IndependenceDay(int year)
-        {
-            return new DateTime(year, 11, 11);
-        }
+        public static DateTime Assumption(int year) => new DateTime(year, 8, 15);
 
         /// <summary>
-        /// Wigilia Bożego Narodzenia (Christmas Eve) - day before the 1st day of Christmas.
-        /// Applies as a public holiday from 2025 onwards.
+        /// All Saints (1st November)
         /// </summary>
-        /// <param name="year"></param>
-        /// <returns></returns>
+        public static DateTime AllSaints(int year) => new DateTime(year, 11, 1);
+
+        /// <summary>
+        /// Independence Day (11th November)
+        /// </summary>
+        public static DateTime IndependenceDay(int year) => new DateTime(year, 11, 11);
+
+        /// <summary>
+        /// Christmas Eve (24th December) - public holiday from 2025 onwards
+        /// </summary>
         public static DateTime? ChristmasEve(int year)
         {
-            if (year >= 2025)
-            {
-                return new DateTime(year, 12, 24);
-            }
-            return null; // Not a public holiday before 2025
+            if (year >= 2025) return new DateTime(year, 12, 24);
+            return null;
         }
 
         /// <summary>
-        /// pierwszy dzien Bozego Narodzenia - 1st day of Christmas
+        /// Christmas Day (25th December)
         /// </summary>
-        /// <param name="year"></param>
-        /// <returns></returns>
-        public static DateTime Christmas(int year)
-        {
-            return new DateTime(year, 12, 25);
-        }
+        public static DateTime Christmas(int year) => new DateTime(year, 12, 25);
 
         /// <summary>
-        /// drugi dzien Bozego Narodzenia - 2nd day of Christmas
+        /// St. Stephen's Day (26th December)
         /// </summary>
-        /// <param name="year"></param>
-        /// <returns></returns>
-        public static DateTime StStephen(int year)
-        {
-            return new DateTime(year, 12, 26);
-        }
+        public static DateTime StStephen(int year) => new DateTime(year, 12, 26);
 
-        #endregion Individual Holidays
+        #endregion
+
+        #region Public Holiday List
 
         /// <summary>
-        /// Get a list of dates for all holidays in a year.
+        /// Get a list of all public holidays in a given year.
         /// </summary>
-        /// <param name="year">The year</param>
-        /// <returns>List of public holidays</returns>
         public override IList<DateTime> PublicHolidays(int year)
         {
             return PublicHolidayNames(year).Select(x => x.Key).OrderBy(x => x).ToList();
         }
 
         /// <summary>
-        /// Public holiday names in Polish.
+        /// Get a dictionary of public holidays with their names in English.
         /// </summary>
-        /// <param name="year">The year.</param>
-        /// <returns></returns>
         public override IDictionary<DateTime, string> PublicHolidayNames(int year)
         {
-            var bHols = new Dictionary<DateTime, string> {
-                { NewYear(year), "Nowy Rok" },
-                { Epiphany(year), "Święto Trzech Króli" } };
-            DateTime easter = HolidayCalculator.GetEaster(year);
-            bHols.Add(EasterMonday(easter), "Poniedziałek Wielkanocny");
-            bHols.Add(LabourDay(year), "Święto Panstwowe");
-            bHols.Add(ConstitutionDay(year), "Święto Narodowe Trzeciego Maja");
-            bHols.Add(CorpusChristi(year),"dzień Bożego Ciała");
-            bHols.Add(Assumption(year), "Wniebowzięcie Najświętszej Maryi Panny");
-            bHols.Add(AllSaints(year), "Wszystkich Swiętych");
-            bHols.Add(IndependenceDay(year), "Narodowe Święto Niepodległości");
+            var holidays = new Dictionary<DateTime, string>
+            {
+                { NewYear(year), "New Year" },
+                { Epiphany(year), "Epiphany" },
+                { EasterSunday(year), "Easter Sunday" },
+                { EasterMonday(year), "Easter Monday" },
+                { LabourDay(year), "Labour Day" },
+                { ConstitutionDay(year), "Constitution Day" },
+                { Pentecost(year), "Pentecost" },
+                { CorpusChristi(year), "Corpus Christi" },
+                { Assumption(year), "Assumption" },
+                { AllSaints(year), "All Saints" },
+                { IndependenceDay(year), "Independence Day" }
+            };
 
-             var christmasEve = ChristmasEve(year);
+            var christmasEve = ChristmasEve(year);
             if (christmasEve.HasValue)
             {
-                bHols.Add(christmasEve.Value, "Wigilia Bożego Narodzenia");
+                holidays.Add(christmasEve.Value, "Christmas Eve");
             }
-            
-            bHols.Add(Christmas(year), "pierwszy dzień Bożego Narodzenia");
-            bHols.Add(StStephen(year), "drugi dzień Bożego Narodzenia");
-            return bHols;
+
+            holidays.Add(Christmas(year), "Christmas");
+            holidays.Add(StStephen(year), "St Stephen's Day");
+
+            return holidays;
         }
+
+        #endregion
+
+        #region Public Holiday Check
 
         /// <summary>
         /// Check if a specific date is a public holiday.
-        /// Obviously the PublicHoliday list is more efficient for repeated checks
-        /// Note holidays can fall on weekends and there is no fixed moving of such dates.
         /// </summary>
-        /// <param name="dt">The date you wish to check</param>
-        /// <returns>True if date is a public holiday</returns>
-        public override bool IsPublicHoliday(DateTime dt)
+        public override bool IsPublicHoliday(DateTime date)
         {
-            var year = dt.Year;
-            var date = dt.Date;
-
-            switch (dt.Month)
-            {
-                case 1:
-                    if (NewYear(year) == date)
-                        return true;
-                    if (Epiphany(year) == date)
-                        return true;
-                    break;
-
-                case 3:
-                case 4:
-                    if (EasterMonday(year) == date)
-                        return true;
-                    break;
-
-                case 5:
-                case 6:
-                    if (LabourDay(year) == date)
-                        return true;
-                    if (ConstitutionDay(year) == date)
-                        return true;
-                    if (CorpusChristi(year) == date)
-                        return true;
-                    break;
-
-                case 8:
-                    if (Assumption(year) == date)
-                        return true;
-                    break;
-
-                case 11:
-                    if (AllSaints(year) == date)
-                        return true;
-                    if (IndependenceDay(year) == date)
-                        return true;
-                    break;
-
-                case 12:
-                     var christmasEve = ChristmasEve(year);
-                    if (christmasEve.HasValue && christmasEve.Value == date)
-                        return true;
-                
-                    if (Christmas(year) == date)
-                        return true;
-                    if (StStephen(year) == date)
-                        return true;
-                    break;
-            }
-
-            return false;
+            return PublicHolidays(date.Year).Contains(date.Date);
         }
+
+        #endregion
     }
 }
