a61a3d069041be3341ad48874bfa5b1872fece79
Eales <46241595+Eales@users.noreply.github.com>
2025-01-03 22:10:13 +0100

    Update TestPolandPublicHoliday.cs

--- tests/PublicHolidayTests/TestPolandPublicHoliday.cs
+++ tests/PublicHolidayTests/TestPolandPublicHoliday.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PublicHoliday;
 using System;
 
@@ -8,7 +8,7 @@ namespace PublicHolidayTests
     public class TestPolandPublicHoliday
     {
         [DataTestMethod]
-        [DataRow(1, 1,"New year")]
+        [DataRow(1, 1, "New Year")]
         [DataRow(1, 6, "Epiphany")]
         [DataRow(4, 17, "Easter Monday")]
         [DataRow(5, 1, "Labour Day")]
@@ -17,7 +17,6 @@ namespace PublicHolidayTests
         [DataRow(8, 15, "Assumption")]
         [DataRow(11, 1, "All Saints")]
         [DataRow(11, 11, "Independence Day")]
-        [DataRow(12, 24, "Christmas Eve")]
         [DataRow(12, 25, "Christmas")]
         [DataRow(12, 26, "St Stephens")]
         public void TestHolidays2017(int month, int day, string name)
@@ -25,7 +24,7 @@ namespace PublicHolidayTests
             var holiday = new DateTime(2017, month, day);
             var holidayCalendar = new PolandPublicHoliday();
             var actual = holidayCalendar.IsPublicHoliday(holiday);
-            Assert.IsTrue(actual, $"{holiday.ToString("D")} is not a holiday -{name}");
+            Assert.IsTrue(actual, $"{holiday.ToString("D")} is not a holiday - {name}");
         }
 
         [TestMethod]
@@ -35,6 +34,37 @@ namespace PublicHolidayTests
             var hols = holidayCalendar.PublicHolidays(2017);
             var holNames = holidayCalendar.PublicHolidayNames(2017);
             Assert.IsTrue(11 == hols.Count, "Should be 11 holidays in 2017");
+            Assert.IsTrue(holNames.Count == hols.Count, "Names and holiday list are same");
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1, "New Year")]
+        [DataRow(1, 6, "Epiphany")]
+        [DataRow(4, 6, "Easter Monday")]
+        [DataRow(5, 1, "Labour Day")]
+        [DataRow(5, 3, "Constitution Day")]
+        [DataRow(6, 8, "Corpus Christi")]
+        [DataRow(8, 15, "Assumption")]
+        [DataRow(11, 1, "All Saints")]
+        [DataRow(11, 11, "Independence Day")]
+        [DataRow(12, 24, "Christmas Eve")]
+        [DataRow(12, 25, "Christmas")]
+        [DataRow(12, 26, "St Stephens")]
+        public void TestHolidays2025(int month, int day, string name)
+        {
+            var holiday = new DateTime(2025, month, day);
+            var holidayCalendar = new PolandPublicHoliday();
+            var actual = holidayCalendar.IsPublicHoliday(holiday);
+            Assert.IsTrue(actual, $"{holiday.ToString("D")} is not a holiday - {name}");
+        }
+
+        [TestMethod]
+        public void TestHolidays2025Lists()
+        {
+            var holidayCalendar = new PolandPublicHoliday();
+            var hols = holidayCalendar.PublicHolidays(2025);
+            var holNames = holidayCalendar.PublicHolidayNames(2025);
+            Assert.IsTrue(12 == hols.Count, "Should be 12 holidays in 2025");
             Assert.IsTrue(holNames.Count == hols.Count, "Names and holiday list are same");
         }
     }
